9c803ab217fbf5d4978a6d3dd696fe3570a907c2
Nils Werner <nils.werner@gmail.com>
2013-06-20 12:03:08 +0200

    Medianlimiter ist teil von Filter. nicht von Statistic.

--- Filter.py
+++ Filter.py
@@ -1,6 +1,10 @@
 import scipy.signal
+import numpy
+import Transform
 
 def lowpass(x, cutoff, fs, coeffs=61):
 	taps = scipy.signal.firwin(coeffs, cutoff/(float(fs)/2), window='hanning')
 	return scipy.signal.lfilter(taps, 1.0, x)
-	
+
+def medianlimiter(x, size=11):
+	return numpy.minimum(x, numpy.median(Transform.slidingwindow(x, size=size), axis=1))
--- Statistic.py
+++ Statistic.py
@@ -1,12 +1,8 @@
 import numpy
-import Transform
 
 def autocorr(x):
 	result = numpy.correlate(x, x, mode='full')
 	return result[result.size/2:]
-
-def medianlimiter(x, size=11):
-	return numpy.minimum(x, numpy.median(Transform.slidingwindow(x, size=size), axis=1))
 
 def quadraticdiff(a, b):
 	return numpy.sum((a - b)**2)
