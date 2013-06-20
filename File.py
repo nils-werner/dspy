e29ab71bec185f3c9e9478464c3718191c8ef761
Nils Werner <nils.werner@gmail.com>
2013-06-20 11:59:37 +0200

    Quatschige maxv sache raus

--- File.py
+++ File.py
@@ -6,13 +6,14 @@ def wavread(filename):
 	with warnings.catch_warnings():
 		warnings.simplefilter("ignore")
 		fs,x = wav.read(filename)
+	maxv = numpy.iinfo(x.dtype).max
 	x = x.astype('float')
-	maxv = numpy.max(numpy.abs(x),axis=0);
 	x = x / maxv * 2
 	x -= 1;
-	return (fs,x,maxv)
+	return (fs,x)
 
-def wavwrite(filename, fs, x, maxv=1):
+def wavwrite(filename, fs, x):
+	maxv = numpy.iinfo(numpy.int16).max
 	x /= numpy.max(numpy.abs(x),axis=0);
 	x += 1;
 	x *= maxv / 2;
