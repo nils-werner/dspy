084238f63e7ad2c7714a31f5c452e6e3f89d9c62
Nils Werner <nils.werner@gmail.com>
2013-06-25 17:51:36 +0200

    Medianlimiter gefixed

--- Filter.py
+++ Filter.py
@@ -6,5 +6,5 @@ def lowpass(x, cutoff, fs, coeffs=61):
 	taps = scipy.signal.firwin(coeffs, cutoff/(float(fs)/2), window='hanning')
 	return scipy.signal.lfilter(taps, 1.0, x)
 
-def medianlimiter(x, size=11):
-	return numpy.minimum(x, numpy.median(Transform.slidingwindow(x, size=size), axis=1))
+def medianlimiter(x, size=11, weight=1):
+	return x / numpy.abs(x) * numpy.minimum(numpy.abs(x), weight*numpy.median(Transform.slidingwindow(numpy.abs(x), size=size), axis=1))
