c986db5ef8d4ce798b454f589d1b7ecd252456a1
Nils Werner <nils.werner@gmail.com>
2013-07-05 13:58:54 +0200

    Programme mit unterschiedlichen Paramtern testen, hat einen Bug aufgedeckt

--- Utils.py
+++ Utils.py
@@ -17,4 +17,4 @@ def powof2(arg):
     num = int(arg)
     if(not(num != 0 and ((num & (num - 1)) == 0))):
         raise argparse.ArgumentTypeError("%r is not a power of two" % num)
-    return value
+    return num
