8ded7ce2b7c8d55f980547c32552fa4f85712d89
Nils Werner <nils.werner@gmail.com>
2013-07-10 18:12:27 +0200

    Noch mehr teager-Zeug, das aber nicht geht

--- Operator.py
+++ Operator.py
@@ -3,3 +3,9 @@ import numpy
 def teager(x):
     e = x[1:-1]**2 - x[2:]*x[:-2]
     return numpy.hstack((e[0], e, e[-1]))
+
+def frequency(x):
+    return numpy.arccos( 1 - ( teager(x[2:]-x[:-2]) / 2*teager(x[1:-1]) ) ) / 2
+
+def amplitude(x):
+    return 2*teager(x[1:-1]) / numpy.sqrt(teager(x[2:]-x[:-2]))
