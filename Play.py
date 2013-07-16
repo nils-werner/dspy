2522ef4c0d6c2aa5c45bd3bc3e2525e9ec3cb6c5
Nils Werner <nils.werner@gmail.com>
2013-07-16 12:55:13 +0200

    Docs fuer Play Modul

--- Play.py
+++ Play.py
@@ -1,3 +1,11 @@
+"""
+Module to play audio signals using portaudio
+
+Functions
+---------
+`play`: Play a signal.
+
+"""
 import pyaudio
 import numpy
 import contextlib
@@ -14,15 +22,37 @@ c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
 
 @contextlib.contextmanager
 def noalsaerr():
+    """
+    A context manager that suppresses ALSA Warnings
+
+    """
     asound = cdll.LoadLibrary('libasound.so')
     asound.snd_lib_error_set_handler(c_error_handler)
     yield
     asound.snd_lib_error_set_handler(None)
 
 def play(sig, rate=44100):
+    """
+    Play a signal.
+
+    Parameters
+    ----------
+    sig : numpy array
+        Input signal.
+    rate : int
+        Sampling rate. Defaults to 44100.
+
+    Notes
+    -----
+
+     * The Input signal is assumed to be a matrix where each row
+       represents one output channel and each column one sample.
+     * The input signal is assumed to be normalized between -1 and 1.
+
+    """
     with noalsaerr():
         p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paFloat32, channels=sig.shape[0], rate=44100, output=1)
+    stream = p.open(format=pyaudio.paFloat32, channels=sig.shape[0], rate=rate, output=1)
     stream.write(numpy.reshape(sig.T, (1,-1)).astype(numpy.float32).tostring())
     stream.close()
     p.terminate()
