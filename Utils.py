85b0b849a9d9eaa0e110a40b47b647fa8a08fec5
Nils Werner <nils.werner@gmail.com>
2013-08-14 11:23:59 +0200

    Median order flag in skripten mit median drin

--- Utils.py
+++ Utils.py
@@ -23,7 +23,6 @@ def defaultopts(parser):
     parser.add_argument('filename', help='The audio file to be processed')
     parser.add_argument('-n', action="store_false", help="Disable graph output", dest="display")
     parser.add_argument('-d', '--figure', metavar="FILENAME", help="Save figure to file")
-    parser.add_argument('-g', '--order', type=int, default=17, metavar="ORDER", help="Set order of median filter, defaults to 17")
 
     parser.add_argument('-f', '--framelength', type=powof2, default=1024, metavar="SAMPLES", help="Set FFT framelength")
     parser.add_argument('--overlap', type=powof2, default=2, metavar="FRAMELENGTH/X", help="Set relative FFT frame overlap")
