698ae74ebc42975c956b324a7d8bb4c0fa64b91b
Nils Werner <nils.werner@gmail.com>
2013-08-27 14:59:21 +0200

    RLS unittest

--- tests/test_Predictor.py
+++ tests/test_Predictor.py
@@ -3,7 +3,7 @@ sys.path.append('..')
 
 from lib import Predictor
 from lib import Statistic
-import scipy, numpy
+import scipy, numpy, scipy.signal
 from pylab import *
 
 def test_levinson():
@@ -21,3 +21,13 @@ def test_prediction():
     corr = Statistic.autocorr(outsig)
     coeffs,pe = Predictor.levinson(corr, 4)
     Predictor.predict(outsig, coeffs)
+
+def test_rls():
+    s = 50000
+    x  = randn(s);
+    b  = scipy.signal.firwin(32,0.5);
+    n  = 0.1*randn(s);
+    d  = scipy.signal.lfilter(b,1,x)+n;
+    lam = 0.99;
+    w,e,y = Predictor.rls(x,d,32,lam)
+    assert numpy.allclose(b,w, rtol=1e-01, atol=1e-01)
