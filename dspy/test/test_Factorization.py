2900b84cea17cb18390c6088874602a4e5a1b2ad
Nils Werner <nils.werner@gmail.com>
2013-10-21 08:57:41 +0200

    NMF unittest

---
+++ dspy/test/test_Factorization.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('..')
+
+import scipy, numpy
+from dspy.dspy import Factorization
+
+def test_nmf():
+    data = numpy.array([[1.0, 0.0, 2.0], [0.0, 1.0, 1.0]])
+    (w, h) = Factorization.nmf(data, iterations=100, num_bases=2)
+    assert numpy.allclose(data, numpy.dot(w,h), rtol=1e-01, atol=1e-01)
