8de136c4a5f03fb3406d1dabce32b6a8207ff6d5
Nils Werner <nils.werner@gmail.com>
2013-11-07 12:41:24 +0100

    Complex teager algorithm

--- dspy/Operator.py
+++ dspy/Operator.py
@@ -1,5 +1,8 @@
 import numpy
 
 def teager(x):
-    e = x[1:-1]**2 - x[2:]*x[:-2]
+    if numpy.iscomplex(x).any():
+        e = teager(numpy.real(x)) + teager(numpy.imag(x))
+    else:
+        e = x[1:-1]**2 - x[2:]*x[:-2]
     return numpy.hstack((e[0], e, e[-1]))
