ba0a20b0f7147aceab1196caea08ac1b82b56c04
Nils Werner <nils.werner@gmail.com>
2013-11-10 19:06:23 +0100

    RMS, DB und iDB

--- dspy/Operator.py
+++ dspy/Operator.py
@@ -6,3 +6,18 @@ def teager(x):
     else:
         e = x[1:-1]**2 - x[2:]*x[:-2]
     return numpy.hstack((e[0], e, e[-1]))
+
+def rms(x, axis=None):
+    return numpy.sqrt(numpy.mean(x**2, axis=axis))
+
+def db(x, energy=False):
+    if energy is False:
+        return 10.0*numpy.log10(x)
+    else:
+        return 20.0*numpy.log10(x)
+
+def idb(x, energy=False):
+    if energy is False:
+        return 10.0 ** (x / 10.0)
+    else:
+        return 10.0 ** (x / 20.0)
